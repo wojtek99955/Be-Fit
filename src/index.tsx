2a56e330589679c9961af8f86633111556797fdf
Wojtek Książek <wojtek99955@gmail.com>
2022-07-01 10:53:23 +0200

    global styles added

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./AuthContext";
+import { GlobalStyles } from "./assets/globalStyles";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,6 +13,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
+        <GlobalStyles />
         <App />
       </AuthProvider>
     </BrowserRouter>
