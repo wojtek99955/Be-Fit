fb389cdb7a7be3e617e8f8af378b8735ead99061
Wojtek Książek <wojtek99955@gmail.com>
2022-07-01 08:18:03 +0200

    routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,49 @@
+import SignUp from "./SignUp";
+import { Routes, Navigate, Route } from "react-router-dom";
+import Home from "./Home";
+import SignIn from "./SignIn";
+import { useContext } from "react";
+import { AuthContext } from "./AuthContext";
+import Data from "./Data";
+import StartPage from "./StartPage";
+
+interface AuthProps {
+  children: JSX.Element;
+}
+
 function App() {
-  return <div className="App"></div>;
+  const ctx = useContext(AuthContext);
+  const islogged = ctx?.currentUser;
+
+  const RequireAuth = ({ children }: AuthProps) => {
+    return islogged ? children : <Navigate to="/signup" />;
+  };
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<StartPage />}>
+          <Route path="signup" element={<SignUp />} />
+          <Route path="signin" element={<SignIn />} />
+        </Route>
+        <Route
+          path="/home"
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/data"
+          element={
+            <RequireAuth>
+              <Data />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
