8a56b8c74685a9e43ce01bdbe111e5166ce4b662
Wojtek Książek <wojtek99955@gmail.com>
2022-07-01 10:45:38 +0200

    content / styles update

--- src/StartPage.tsx
+++ src/StartPage.tsx
@@ -1,12 +1,39 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import img from "./assets/images/main.jpg";
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.4) url(${img});
+  background-blend-mode: darken;
+  background-size: cover;
+`;
+
+const Main = styled.main`
+  h1 {
+    text-align: center;
+    color: white;
+  }
+  button {
+    background-color: blue;
+    border: none;
+    color: white;
+    padding: 1rem 2.3rem;
+    display: block;
+    margin: auto;
+  }
+`;
 
 const StartPage = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-      <Outlet />
-    </div>
+    <Container>
+      <Main>
+        <h1>Track your calorie intake with us!</h1>
+        <button>Sign In</button>
+      </Main>
+    </Container>
   );
 };
 
