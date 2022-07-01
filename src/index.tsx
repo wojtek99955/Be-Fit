644feecbe6aec65ab71ef3f17510d8095d974edf
Wojtek Książek <wojtek99955@gmail.com>
2022-07-01 08:18:37 +0200

    router added

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./AuthContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
