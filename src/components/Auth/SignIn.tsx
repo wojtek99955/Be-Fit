fc334f52d7f37067ce37141ca80542dc31bc4cf9
Wojtek Książek <wojtek99955@gmail.com>
2022-07-03 12:21:38 +0200

    form update

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -4,13 +4,7 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { Formik, Form, ErrorMessage } from "formik";
 import Loader from "../../assets/Loader";
-import {
-  Container,
-  FormContainer,
-  Label,
-  StyledField,
-  StyledLink,
-} from "./AuthStyle";
+import { FormContainer, Label, StyledField, StyledLink } from "./AuthStyle";
 import ValidationError from "./ValidationError";
 import * as yup from "yup";
 
@@ -26,38 +20,32 @@ const SignIn = () => {
   let navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   return (
-    <Container>
-      <FormContainer>
-        <h2>Sign In</h2>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={async (values) => {
-            setLoading(true);
-            await signInWithEmailAndPassword(
-              auth,
-              values.email,
-              values.password
-            );
-            setLoading(false);
-            navigate("/home");
-          }}
-        >
-          <Form>
-            <Label htmlFor="email">email</Label>
-            <StyledField type="email" name="email" id="email" />
-            <ErrorMessage name="email" component={ValidationError} />
-            <Label htmlFor="password">password</Label>
-            <StyledField type="password" name="password" id="password" />
-            <ErrorMessage name="password" component={ValidationError} />
+    <FormContainer>
+      <h2>Sign In</h2>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={async (values) => {
+          setLoading(true);
+          await signInWithEmailAndPassword(auth, values.email, values.password);
+          setLoading(false);
+          navigate("/home");
+        }}
+      >
+        <Form>
+          <Label htmlFor="email">email</Label>
+          <StyledField type="email" name="email" id="email" />
+          <ErrorMessage name="email" component={ValidationError} />
+          <Label htmlFor="password">password</Label>
+          <StyledField type="password" name="password" id="password" />
+          <ErrorMessage name="password" component={ValidationError} />
 
-            <button type="submit">SignUp</button>
-          </Form>
-        </Formik>
-        <StyledLink to="/data"> Don't have an account? Sign up!</StyledLink>
-        {loading && <Loader />}
-      </FormContainer>
-    </Container>
+          <button type="submit">SignUp</button>
+        </Form>
+      </Formik>
+      <StyledLink to="/data"> Don't have an account? Sign up!</StyledLink>
+      {loading && <Loader />}
+    </FormContainer>
   );
 };
 
