80f755e1c644898a96607527cd175f67df00b658
Wojtek Książek <wojtek99955@gmail.com>
2022-07-05 15:53:11 +0200

    content / styles update

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,17 +1,55 @@
 import styled from "styled-components";
+import { UserIcon } from "./Header/HeaderStyle";
+import { Link } from "react-router-dom";
 
 const Container = styled.aside`
   height: 100vh;
-  width: 300px;
+  width: 250px;
   border-right: 1px solid #e1e4e7;
   position: relative;
-  z-index: 1;
   top: 5.2rem;
   height: calc(100vh - 5.2rem);
+  padding: 0.8rem;
+  ul {
+    list-style-type: none;
+  }
+  li {
+    padding: 0.8rem 0;
+    cursor: pointer;
+    &:hover {
+      background-color: #e4e7e8;
+    }
+  }
+`;
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  font-size: 0.9rem;
+  font-weight: 400;
 `;
 
 const SideBar = () => {
-  return <Container>SideBar</Container>;
+  return (
+    <Container>
+      <UserIcon>K</UserIcon>
+      <nav>
+        <ul>
+          <li>
+            <StyledLink to="/">Home page</StyledLink>
+          </li>
+          <li>
+            <StyledLink to="/">Your goals</StyledLink>
+          </li>
+          <li>
+            <StyledLink to="/">Calculators</StyledLink>
+          </li>
+          <li>
+            <StyledLink to="/">Check calories</StyledLink>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  );
 };
 
 export default SideBar;
