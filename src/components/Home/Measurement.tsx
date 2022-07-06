77e838e9fe187cbd28451f197fce24ee2ed6ac4a
Wojtek Książek <wojtek99955@gmail.com>
2022-07-06 13:08:59 +0200

    styles update

--- src/components/Home/Measurement.tsx
+++ src/components/Home/Measurement.tsx
@@ -5,7 +5,65 @@ import styled from "styled-components";
 import { AuthContext } from "../AuthContext";
 
 const Box = styled.div`
-  border: 1px solid grey;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 12px;
+  h2 {
+    font-size: 1rem;
+    margin-bottom: 1rem;
+  }
+`;
+const IconContainer = styled.div`
+  width: 2.8rem;
+  height: 2.8rem;
+  border-radius: 50%;
+  margin-left: 1rem;
+`;
+
+const DataContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.5rem 0;
+  border-radius: 8px;
+
+  &:first-of-type {
+    background-color: #ffe9c5;
+  }
+  &:nth-of-type(2) {
+    background-color: #c8dce1;
+  }
+  &:nth-of-type(3) {
+    background-color: #eef7f9;
+  }
+`;
+
+const DataWrapper = styled.div`
+  strong {
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+  p {
+    color: #bcbcbc;
+  }
+`;
+
+const AgeIconContainer = styled(IconContainer)`
+  background-color: #ffa101;
+`;
+
+const HeightIconContainer = styled(IconContainer)`
+  background-color: #31525b;
+`;
+
+const WeightIconContainer = styled(IconContainer)`
+  background-color: #b3dee5;
+`;
+
+const BoxWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
 `;
 
 const Measurement = () => {
@@ -33,9 +91,29 @@ const Measurement = () => {
   return (
     <Box>
       <h2>Currently</h2>
-      <p>Age: {age}</p>
-      <p>Height: {height}</p>
-      <p>Weight: {weight}</p>
+      <BoxWrapper>
+        <DataContainer>
+          <AgeIconContainer></AgeIconContainer>
+          <DataWrapper>
+            <strong>Age</strong>
+            <p>{age}</p>
+          </DataWrapper>
+        </DataContainer>
+        <DataContainer>
+          <HeightIconContainer></HeightIconContainer>
+          <DataWrapper>
+            <strong>Height</strong>
+            <p>{height}</p>
+          </DataWrapper>
+        </DataContainer>
+        <DataContainer>
+          <WeightIconContainer></WeightIconContainer>
+          <DataWrapper>
+            <strong>Weight</strong>
+            <p> {weight}</p>
+          </DataWrapper>
+        </DataContainer>
+      </BoxWrapper>
     </Box>
   );
 };
