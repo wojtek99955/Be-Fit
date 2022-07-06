c50c87e231baa6e96b24372a0b0c1747a7532c24
Wojtek Książek <wojtek99955@gmail.com>
2022-07-06 10:12:35 +0200

    content update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useContext } from "react";
 import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, getDoc, doc } from "firebase/firestore";
 import { db } from "../firebase";
 import styled from "styled-components";
 import { AuthContext } from "./AuthContext";
@@ -12,8 +12,13 @@ const Container = styled.section`
   padding-top: 8rem;
 `;
 
+const Box = styled.div`
+  border: 1px solid grey;
+`;
+
 const Home = () => {
   const ctx = useContext(AuthContext);
+  const uid = ctx?.currentUser.uid;
   const authData = useAuth();
   let navigate = useNavigate();
   const logOut = async () => {
@@ -22,40 +27,34 @@ const Home = () => {
   };
 
   useEffect(() => {
-    getFood();
+    getData();
   }, []);
 
-  const [food, setFood] = useState<any>([]);
-  function getFood() {
-    const foodCollectionRef = collection(
-      db,
-      "benefit-bdeb9/",
-      "food/",
-      "orange"
-    );
-    getDocs(foodCollectionRef)
-      .then((res) => {
-        setFood(res);
-      })
-      .catch((error) => console.log(error.message));
+  const [data, setData] = useState<any>([]);
+
+  async function getData() {
+    const snap = await getDoc(doc(db, "users", `${uid}/body-details/details`));
+
+    if (snap.exists()) {
+      console.log(snap.data());
+      setData(snap.data());
+    } else {
+      console.log("No such document");
+    }
   }
-
-  console.log(ctx?.currentUser);
-  const dbx = getDatabase();
-
+  console.log(typeof data.age);
+  const sum = data.height + data.age;
+  const BMI = data.weight / Math.pow(data.height / 100, 2);
+  const { height, age, weight } = data;
   return (
     <Container>
-      <h1>youre log in</h1>
-      {/* {food.map((item: any) => {
-        return (
-          <>
-            <h1>{item.id}</h1>
-            <h2>{item.data.kcal}</h2>
-          </>
-        );
-      })} */}
-      <h2>Your email is {authData?.currentUser.email}</h2>
-      <button onClick={logOut}>Log out</button>
+      <Box>
+        <h2>Your current measure</h2>
+        <p>Age: {age}</p>
+        <p>Height: {height}</p>
+        <p>Weight: {weight}</p>
+        <p>bmi : {BMI.toFixed(1)}</p>
+      </Box>
     </Container>
   );
 };
