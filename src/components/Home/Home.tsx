42487eb6efc80fe87e56c54bb45ee60ac43935e4
Wojtek Książek <wojtek99955@gmail.com>
2022-07-06 11:57:06 +0200

    styles update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,17 +1,20 @@
 import styled from "styled-components";
-import BMI from "./BMI";
+import Measurement from "./Measurement";
 import Greeting from "./Greeting";
+import BMI from "./BMI";
 
 const Container = styled.section`
   padding: 1rem;
   padding-top: 8rem;
   width: 100%;
+  background-color: #f5f2f6;
 `;
 
 const Home = () => {
   return (
     <Container>
       <Greeting />
+      <Measurement />
       <BMI />
     </Container>
   );
