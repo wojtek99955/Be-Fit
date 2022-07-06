5a24b2014384ebc76ff597ca2922a2a743b274c2
Wojtek Książek <wojtek99955@gmail.com>
2022-07-06 10:56:34 +0200

    greetings added

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,17 @@
 import styled from "styled-components";
 import BMI from "./BMI";
+import Greeting from "./Greeting";
 
 const Container = styled.section`
+  padding: 1rem;
   padding-top: 8rem;
+  width: 100%;
 `;
 
 const Home = () => {
   return (
     <Container>
+      <Greeting />
       <BMI />
     </Container>
   );
