5edccf536fde3d13373f86a09c495fc8a13d7419
Wojtek Książek <wojtek99955@gmail.com>
2022-07-06 15:24:15 +0200

    styles update

--- src/components/Home/Goal.tsx
+++ src/components/Home/Goal.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { Box, SettingsIcon } from "./CardStyles";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 1rem;
+  border-radius: 12px;
+  &:first-of-type {
+    background-color: #faf2ef;
+    p {
+      background-color: #e1605e;
+    }
+  }
+  &:nth-of-type(2) {
+    background-color: #c7e1c7;
+    p {
+      background-color: #6db26b;
+    }
+  }
+  h2 {
+    font-size: 1rem;
+    color: #bcbcbc;
+    margin-bottom: 1rem;
+  }
+  p {
+    font-weight: 400;
+    font-size: 1.2rem;
+    display: inline-block;
+    color: white;
+    padding: 0.3rem;
+    border-radius: 12px;
+  }
+`;
+
+const StyledBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+const StyledSettingsIcon = styled(SettingsIcon)`
+  margin-left: auto;
+`;
 
 const Goal = () => {
-  return <div>Goal</div>;
+  return (
+    <StyledBox>
+      <StyledSettingsIcon />
+      <Wrapper>
+        <h2>Current weight</h2>
+        <p>80 kg</p>
+      </Wrapper>
+      <Wrapper>
+        <h2>Target weight</h2>
+        <p>76 kg</p>
+      </Wrapper>
+    </StyledBox>
+  );
 };
 
 export default Goal;
