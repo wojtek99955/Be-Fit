1d36932153fe6402c7150c9489d557f54f1ea3b1
Wojtek Książek <wojtek99955@gmail.com>
2022-07-06 13:08:38 +0200

    Styles changed

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Measurement from "./Measurement";
 import Greeting from "./Greeting";
 import BMI from "./BMI";
+import Goal from "./Goal";
 
 const Container = styled.section`
   padding: 1rem;
@@ -9,13 +10,21 @@ const Container = styled.section`
   width: 100%;
   background-color: #f5f2f6;
 `;
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 2rem;
+`;
 
 const Home = () => {
   return (
     <Container>
       <Greeting />
-      <Measurement />
-      <BMI />
+      <GridContainer>
+        <Measurement />
+        <BMI />
+        <Goal />
+      </GridContainer>
     </Container>
   );
 };
