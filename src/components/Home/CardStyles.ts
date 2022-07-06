fe375a4e09bf6ae18b7d2fc05c7bfc0a8617b0b3
Wojtek Książek <wojtek99955@gmail.com>
2022-07-06 14:31:40 +0200

    styles refactor

---
+++ src/components/Home/CardStyles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { GoSettings } from "react-icons/go";
+import { Link } from "react-router-dom";
+
+export const Box = styled.div`
+  background-color: white;
+  padding: 1rem;
+  border-radius: 12px;
+`;
+
+export const SettingsIcon = styled(GoSettings)`
+  font-size: 1.5rem;
+  color: #bcbcbc;
+  cursor: pointer;
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
