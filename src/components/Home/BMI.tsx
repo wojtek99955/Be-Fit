f8c5024458cee08f4674aebb6c4b3c3eb654c65f
Wojtek Książek <wojtek99955@gmail.com>
2022-07-06 14:20:44 +0200

    bmi styles logic added

--- src/components/Home/BMI.tsx
+++ src/components/Home/BMI.tsx
@@ -4,9 +4,47 @@ import { db } from "../../firebase";
 import styled from "styled-components";
 import { AuthContext } from "../AuthContext";
 
+interface StyleProps {
+  bmi: number;
+}
 const Box = styled.div`
-  border: 1px solid grey;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+const Wrapper = styled.div<StyleProps>`
+  border-width: 8px;
+  border-style: solid;
+  border-color: ${({ bmi }) => {
+    if (bmi >= 25) {
+      return "#E1605E";
+    } else if (bmi >= 18.5) {
+      return "#6DB26B";
+    } else if (bmi < 18.5) {
+      return "#E1605E";
+    } else {
+      return "transparent";
+    }
+  }};
+  width: 10rem;
+  height: 10rem;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p {
+    color: #bcbcbc;
+    text-align: center;
+  }
+`;
+const Bmi = styled.strong`
+  font-size: 2.5rem;
+  display: block;
+`;
+const Data = styled.div``;
 
 const BMI = () => {
   const ctx = useContext(AuthContext);
@@ -31,8 +69,12 @@ const BMI = () => {
   const BMI = weight / Math.pow(height / 100, 2);
   return (
     <Box>
-      <h3>Your BMI</h3>
-      <div>{BMI.toFixed(1)}</div>
+      <Wrapper bmi={BMI}>
+        <Data>
+          <Bmi>{BMI.toFixed(1)}</Bmi>
+          <p>BMI</p>
+        </Data>
+      </Wrapper>
     </Box>
   );
 };
