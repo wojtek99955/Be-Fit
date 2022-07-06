2765c8b32fb9d78cf44e8603f688b695b63d800f
Wojtek Książek <wojtek99955@gmail.com>
2022-07-06 13:20:45 +0200

    styles update

--- src/components/Home/Measurement.tsx
+++ src/components/Home/Measurement.tsx
@@ -3,21 +3,24 @@ import { getDoc, doc } from "firebase/firestore";
 import { db } from "../../firebase";
 import styled from "styled-components";
 import { AuthContext } from "../AuthContext";
+import { BsCalendar3 } from "react-icons/bs";
+import { GiBodyHeight } from "react-icons/gi";
+import { GiWeight } from "react-icons/gi";
+import { GoSettings } from "react-icons/go";
 
 const Box = styled.div`
   background-color: white;
   padding: 1rem;
   border-radius: 12px;
-  h2 {
-    font-size: 1rem;
-    margin-bottom: 1rem;
-  }
 `;
 const IconContainer = styled.div`
   width: 2.8rem;
   height: 2.8rem;
   border-radius: 50%;
   margin-left: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const DataContainer = styled.div`
@@ -66,6 +69,34 @@ const BoxWrapper = styled.div`
   gap: 0.5rem;
 `;
 
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+  h2 {
+    font-size: 1rem;
+  }
+`;
+
+const CalendarIcon = styled(BsCalendar3)`
+  font-size: 1rem;
+  color: white;
+`;
+const HeightIcon = styled(GiBodyHeight)`
+  font-size: 1rem;
+  color: white;
+`;
+const WeightIcon = styled(GiWeight)`
+  font-size: 1.2rem;
+  color: white;
+`;
+const SettingsIcon = styled(GoSettings)`
+  font-size: 1.5rem;
+  color: #bcbcbc;
+  cursor: pointer;
+`;
+
 const Measurement = () => {
   const ctx = useContext(AuthContext);
   const uid = ctx?.currentUser.uid;
@@ -90,24 +121,33 @@ const Measurement = () => {
 
   return (
     <Box>
-      <h2>Currently</h2>
+      <Header>
+        <h2>Currently</h2>
+        <SettingsIcon />
+      </Header>
       <BoxWrapper>
         <DataContainer>
-          <AgeIconContainer></AgeIconContainer>
+          <AgeIconContainer>
+            <CalendarIcon />
+          </AgeIconContainer>
           <DataWrapper>
             <strong>Age</strong>
             <p>{age}</p>
           </DataWrapper>
         </DataContainer>
         <DataContainer>
-          <HeightIconContainer></HeightIconContainer>
+          <HeightIconContainer>
+            <HeightIcon />
+          </HeightIconContainer>
           <DataWrapper>
             <strong>Height</strong>
             <p>{height}</p>
           </DataWrapper>
         </DataContainer>
         <DataContainer>
-          <WeightIconContainer></WeightIconContainer>
+          <WeightIconContainer>
+            <WeightIcon />
+          </WeightIconContainer>
           <DataWrapper>
             <strong>Weight</strong>
             <p> {weight}</p>
