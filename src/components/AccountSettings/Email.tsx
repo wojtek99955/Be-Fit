7599c097eb6044c2fe6e56f1b6b7e261f91f7b3b
Wojtek Książek <wojtek99955@gmail.com>
2022-07-11 11:45:39 +0200

    email settings added

---
+++ src/components/AccountSettings/Email.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+const Container = styled.div`
+  padding-top: 10rem;
+`;
+const Email = () => {
+  return <Container>Email</Container>;
+};
+
+export default Email;
