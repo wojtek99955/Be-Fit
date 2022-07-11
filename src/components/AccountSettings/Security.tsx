da22b61349c3776766a501ed5e0072e8c72a535c
Wojtek Książek <wojtek99955@gmail.com>
2022-07-11 13:11:56 +0200

    delete account added

--- src/components/AccountSettings/Security.tsx
+++ src/components/AccountSettings/Security.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Container, Button } from "./AccountSettingsStyle";
+import styled from "styled-components";
+import { deleteUser } from "firebase/auth";
+import { AuthContext } from "../AuthContext";
+import { useContext } from "react";
+
+const StyledButton = styled(Button)``;
 
 const Security = () => {
-  return <div>Security</div>;
+  const ctx = useContext(AuthContext);
+  return (
+    <Container>
+      <h3>Delete your account</h3>
+      <StyledButton
+        onClick={() => {
+          deleteUser(ctx?.currentUser).then(() => console.log("delete"));
+        }}
+      >
+        Delete
+      </StyledButton>
+    </Container>
+  );
 };
 
 export default Security;
