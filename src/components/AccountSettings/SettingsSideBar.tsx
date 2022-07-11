28146b4446c7920f7060eae1685482d91b78f3af
Wojtek Książek <wojtek99955@gmail.com>
2022-07-11 10:52:26 +0200

    content update

---
+++ src/components/AccountSettings/SettingsSideBar.tsx
@@ -0,0 +1,48 @@
+import { Container, StyledLink } from "../SideBar/SideBarStyle";
+import SideBarUserData from "../SideBarUserData";
+import styled from "styled-components";
+import { BiUserCircle } from "react-icons/bi";
+import { BiLock } from "react-icons/bi";
+import { HiOutlineMail } from "react-icons/hi";
+
+const UserIcon = styled(BiUserCircle)`
+  font-size: 1.3rem;
+`;
+const LockIcon = styled(BiLock)`
+  font-size: 1.3rem;
+`;
+const EmailIcon = styled(HiOutlineMail)`
+  font-size: 1.3rem;
+`;
+
+const SettingsSideBar = () => {
+  return (
+    <Container>
+      <SideBarUserData />
+      <nav>
+        <ul>
+          <li>
+            <StyledLink to="/">
+              <UserIcon />
+              Your account
+            </StyledLink>
+          </li>
+          <li>
+            <StyledLink to="/">
+              <LockIcon />
+              Security
+            </StyledLink>
+          </li>
+          <li>
+            <StyledLink to="/">
+              <EmailIcon />
+              Email preferences
+            </StyledLink>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  );
+};
+
+export default SettingsSideBar;
