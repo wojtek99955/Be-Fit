a9781140e93aa2f3da17b979d9447fa45345c434
Wojtek Książek <wojtek99955@gmail.com>
2022-07-14 13:33:55 +0200

    formik added

--- src/components/Calculators/CalorieIntake.tsx
+++ src/components/Calculators/CalorieIntake.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import styled from "styled-components";
+import { Formik, Form, Field } from "formik";
+
+const Container = styled.section``;
+
+const initialValues = {
+  gender: "",
+  weight: "",
+  height: "",
+  age: "",
+  activityLevel: "",
+};
 
 const CalorieIntake = () => {
-  return <div>CalorieIntake</div>;
+  return (
+    <Container>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => console.log(values)}
+      >
+        <Form>
+          <label htmlFor="gender">Gender</label>
+          <Field name="gender" placeholder="gender" id="gender" />
+          <label htmlFor="age">Age</label>
+          <Field name="age" placeholder="age" id="age" />
+          <label htmlFor="weight">Weight</label>
+          <Field name="weight" placeholder="weight" />
+          <label htmlFor="height">Height</label>
+          <Field name="height" id="height" />
+          <button>Get result</button>
+        </Form>
+      </Formik>
+    </Container>
+  );
 };
 
 export default CalorieIntake;
