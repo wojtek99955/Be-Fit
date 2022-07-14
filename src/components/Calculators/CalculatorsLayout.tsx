2f15794880289b79358deefb6e594b6d0ec67f13
Wojtek Książek <wojtek99955@gmail.com>
2022-07-14 09:56:19 +0200

    styles update

--- src/components/Calculators/CalculatorsLayout.tsx
+++ src/components/Calculators/CalculatorsLayout.tsx
@@ -2,7 +2,11 @@ import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.section`
-  width: 100%;
+  width: calc(100vw - 14rem);
+  height: calc(100vh - 3.5rem);
+  position: relative;
+  top: 3.5rem;
+  overflow: scroll;
   display: flex;
   justify-content: center;
 `;
