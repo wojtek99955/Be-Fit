f519567b44b66ba770d6ab8dd0ebbc4891a4b0ef
Wojtek Książek <wojtek99955@gmail.com>
2022-07-14 13:55:28 +0200

    form update

--- src/components/Calculators/CalorieIntake.tsx
+++ src/components/Calculators/CalorieIntake.tsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 import { Formik, Form, Field } from "formik";
 
-const Container = styled.section``;
+const Container = styled.section`
+  label {
+    display: block;
+  }
+`;
 
 const initialValues = {
   gender: "",
@@ -11,6 +15,19 @@ const initialValues = {
   activityLevel: "",
 };
 
+const Row = styled.div`
+  display: flex;
+`;
+const InputContainer = styled.div``;
+
+enum Activity {
+  sedentaryLifestyle = "sedentary lifestyle",
+  littleActive = "little active lifestyle",
+  moderateActivity = "moderate physical activity",
+  veryActive = "very active lifestyle",
+  sport = "sport lifestyle",
+}
+
 const CalorieIntake = () => {
   return (
     <Container>
@@ -19,14 +36,39 @@ const CalorieIntake = () => {
         onSubmit={(values) => console.log(values)}
       >
         <Form>
-          <label htmlFor="gender">Gender</label>
-          <Field name="gender" placeholder="gender" id="gender" />
-          <label htmlFor="age">Age</label>
-          <Field name="age" placeholder="age" id="age" />
-          <label htmlFor="weight">Weight</label>
-          <Field name="weight" placeholder="weight" />
-          <label htmlFor="height">Height</label>
-          <Field name="height" id="height" />
+          <Row>
+            <InputContainer>
+              <label htmlFor="gender">Gender</label>
+              <Field name="gender" placeholder="gender" id="gender" />
+            </InputContainer>
+            <InputContainer>
+              <label htmlFor="age">Age</label>
+              <Field name="age" placeholder="age" id="age" />
+            </InputContainer>
+          </Row>
+          <Row>
+            <InputContainer>
+              <label htmlFor="weight">Weight</label>
+              <Field name="weight" placeholder="weight" />
+            </InputContainer>
+            <InputContainer>
+              <label htmlFor="height">Height</label>
+              <Field name="height" id="height" placeholder="height" />
+            </InputContainer>
+          </Row>
+          <Field as="select" name="activity">
+            <option value={Activity.littleActive}>
+              {Activity.littleActive}
+            </option>
+            <option value={Activity.sedentaryLifestyle}>
+              {Activity.sedentaryLifestyle}
+            </option>
+            <option value={Activity.moderateActivity}>
+              {Activity.moderateActivity}
+            </option>
+            <option value={Activity.veryActive}>{Activity.veryActive}</option>
+            <option value={Activity.sport}>{Activity.sport}</option>
+          </Field>
           <button>Get result</button>
         </Form>
       </Formik>
