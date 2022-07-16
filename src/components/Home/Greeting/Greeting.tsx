1e28f89aff254bcc4fd80810efb5ce0f01062e22
Wojtek Książek <wojtek99955@gmail.com>
2022-07-16 09:33:38 +0200

    svg added

--- src/components/Home/Greeting/Greeting.tsx
+++ src/components/Home/Greeting/Greeting.tsx
@@ -1,9 +1,31 @@
 import { Container, SearchInput } from "./GreetingStyle";
+import HealthyStyleIcon from "../../../assets/svg/HealthyStyleIcon";
+import styled from "styled-components";
+import WorkOutIcon from "../../../assets/svg/WorkOutIcon";
+
+const HealthyStyleIconContainer = styled.div`
+  width: 10rem;
+  position: absolute;
+  left: 3rem;
+`;
+
+const WorkOutIconContainer = styled.div`
+  width: 8rem;
+  position: absolute;
+  right: 3rem;
+`;
 
 const Greeting = () => {
   return (
     <Container>
-      <h1>Good To See You!</h1>
+      <WorkOutIconContainer>
+        <WorkOutIcon />
+      </WorkOutIconContainer>
+      <HealthyStyleIconContainer>
+        <HealthyStyleIcon />
+      </HealthyStyleIconContainer>
+
+      <h1 style={{ position: "relative", zIndex: "2" }}>Good To See You!</h1>
       <SearchInput placeholder="Search" />
     </Container>
   );
