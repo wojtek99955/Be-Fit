10e3ca66460ca9e3d1b02a54911c1778acd47fe2
Wojtek Książek <wojtek99955@gmail.com>
2022-07-19 15:01:26 +0200

    content / styles update

--- src/components/TrackCalories/TrackCalories.tsx
+++ src/components/TrackCalories/TrackCalories.tsx
@@ -1,5 +1,42 @@
+import styled from "styled-components";
+
+const img = require("../../assets/images/track-calories.jpg");
+
+const Container = styled.section`
+  padding: 1rem;
+  width: calc(100vw - 14rem);
+  background-color: #f5f2f6;
+  height: calc(100vh - 3.5rem);
+  position: relative;
+  top: 3.5rem;
+  overflow-y: scroll;
+`;
+const Header = styled.div`
+  width: 100%;
+  background: rgba(0, 0, 0, 0.3) url(${img});
+  height: 20rem;
+  background-position: bottom;
+  background-size: cover;
+  background-blend-mode: darken;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  h2 {
+    color: white;
+    font-size: 2rem;
+  }
+`;
+
 const TrackCalories = () => {
-  return <div>TrackCalories</div>;
+  return (
+    <Container>
+      <Header>
+        <h2>Track your daily calorie intake</h2>
+      </Header>
+    </Container>
+  );
 };
 
 export default TrackCalories;
