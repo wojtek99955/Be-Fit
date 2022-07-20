6ab4e48de6f44bba0536697bcead9d8a6b4e5233
Wojtek Książek <wojtek99955@gmail.com>
2022-07-20 10:15:25 +0200

    formik added

--- src/components/YourGoal/YourGoal.tsx
+++ src/components/YourGoal/YourGoal.tsx
@@ -1,5 +1,95 @@
+import { Formik, Form, Field } from "formik";
+import styled from "styled-components";
+
+const Container = styled.section`
+  margin-top: 6rem;
+`;
+
+const Row = styled.div`
+  display: flex;
+`;
+
+const initialValues = {
+  currentWeight: "",
+  goalWeight: "",
+  calorieDeficit: 100,
+};
+
+const StyledField = styled(Field)`
+  width: 15rem;
+  display: block;
+  font-size: 1.3rem;
+  padding: 0.2rem;
+`;
+
+const FormContainer = styled.div`
+  margin-top: 3rem;
+`;
+
+const RangeInput = styled.div`
+  width: 100%;
+  margin-top: 2rem;
+
+  input {
+    -webkit-appearance: none;
+    width: 100%;
+    height: 1rem;
+    border-radius: 5px;
+    background: #d3d3d3;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: 0.2s;
+    transition: opacity 0.2s;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 1;
+    }
+    &::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      appearance: none;
+      height: 2rem;
+      width: 2rem;
+      border-radius: 50%;
+      background-color: #ffa101;
+    }
+    &::-moz-range-thumb {
+      width: 1rem;
+      height: 1rem;
+      background: #04aa6d;
+      cursor: pointer;
+    }
+  }
+`;
+
 const YourGoal = () => {
-  return <div>YourGoal</div>;
+  return (
+    <Container>
+      <h1>Your goal </h1>
+      <FormContainer>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(values) => console.log(values)}
+        >
+          <Form>
+            <Row>
+              <StyledField name="currentWeight" placeholder="current weight" />
+              <StyledField name="goal weight" placeholder="goal weight" />
+            </Row>
+            <RangeInput>
+              <Field
+                type="range"
+                min="100"
+                max="1000"
+                step="50"
+                name="calorieDeficit"
+              />
+            </RangeInput>
+          </Form>
+        </Formik>
+      </FormContainer>
+    </Container>
+  );
 };
 
 export default YourGoal;
