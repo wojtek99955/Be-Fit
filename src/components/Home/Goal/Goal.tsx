d180c493f10fce50b1e4cba5aab9623d0030c0d7
Wojtek Książek <wojtek99955@gmail.com>
2022-07-21 09:32:43 +0200

    data added

--- src/components/Home/Goal/Goal.tsx
+++ src/components/Home/Goal/Goal.tsx
@@ -1,7 +1,33 @@
 import { Wrapper, StyledBox, StyledSettingsIcon } from "./GoalStyle";
 import { StyledLink } from "../CardStyles";
+import { useEffect, useState, useContext } from "react";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../../../firebase";
+import { AuthContext } from "../../AuthContext";
 
 const Goal = () => {
+  const [loading, setLoading] = useState(true);
+  const ctx = useContext(AuthContext);
+  const uid = ctx?.currentUser.uid;
+  const [data, setData] = useState<any>("");
+
+  async function getData() {
+    const snap = await getDoc(doc(db, "users", `${uid}/body-details/goals`));
+
+    if (snap.exists()) {
+      setData(snap.data());
+      setLoading(false);
+    } else {
+      console.log("No such document");
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(data);
+
   return (
     <StyledBox>
       <StyledLink to="/my-goal">
@@ -9,11 +35,11 @@ const Goal = () => {
       </StyledLink>
       <Wrapper>
         <h2>Current weight</h2>
-        <p>80 kg</p>
+        <p>{data.currentWeight}</p>
       </Wrapper>
       <Wrapper>
         <h2>Target weight</h2>
-        <p>76 kg</p>
+        <p>{data.goalWeight}</p>
       </Wrapper>
     </StyledBox>
   );
