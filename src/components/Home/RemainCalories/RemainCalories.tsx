0c3582dc41b775c77b1c397e2120b18028797eea
Wojtek Książek <wojtek99955@gmail.com>
2022-07-25 12:18:42 +0200

    data added

--- src/components/Home/RemainCalories/RemainCalories.tsx
+++ src/components/Home/RemainCalories/RemainCalories.tsx
@@ -1,7 +1,56 @@
-import styled from "styled-components";
+import { Box } from "../CardStyles";
+import { useEffect, useState, useContext } from "react";
+import { AuthContext } from "../../AuthContext";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../../../firebase";
 
 const RemainCalories = () => {
-  return <div>RemainCalories</div>;
+  const ctx = useContext(AuthContext);
+  const uid = ctx?.currentUser.uid;
+  const [consumedKcal, setConsumedKcal] = useState<null | number>(null);
+  const [calorieIntake, setCalorieIntake] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  async function getConsumedKcal() {
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    const snap = await getDoc(
+      doc(db, "users", `${uid}/consumedNutrients/${day}${month}${year}`)
+    );
+
+    if (snap.exists()) {
+      setConsumedKcal(snap.data().kcal);
+    } else {
+      console.log("No such document");
+    }
+  }
+
+  async function getCalorieIntake() {
+    const snap = await getDoc(
+      doc(db, "users", `${uid}/body-details/calorie-intake`)
+    );
+
+    if (snap.exists()) {
+      setCalorieIntake(snap.data().calorieIntake);
+    } else {
+      console.log("No such document");
+    }
+  }
+
+  useEffect(() => {
+    getConsumedKcal();
+    getCalorieIntake();
+  }, []);
+
+  return (
+    <Box>
+      RemainCalories
+      <h1>{calorieIntake - consumedKcal!}</h1>
+    </Box>
+  );
 };
 
 export default RemainCalories;
