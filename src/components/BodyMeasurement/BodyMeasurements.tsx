33791f4f9fc4acded59acd998bb50838e463ad81
Wojtek Książek <wojtek99955@gmail.com>
2022-07-29 09:23:30 +0200

    validation added

--- src/components/BodyMeasurement/BodyMeasurements.tsx
+++ src/components/BodyMeasurement/BodyMeasurements.tsx
@@ -1,9 +1,11 @@
-import { Formik, Form } from "formik";
+import { Formik, Form, ErrorMessage } from "formik";
 import { setDoc, doc } from "firebase/firestore";
 import { db } from "../../firebase";
 import { AuthContext } from "../AuthContext";
 import { useContext } from "react";
 import { Container, Wrapper, Row, StyledField } from "./BodyMeasurementStyle";
+import * as yup from "yup";
+import { ErrorMsg } from "../Auth/AuthStyle";
 
 const initialValues = {
   gender: "",
@@ -11,6 +13,27 @@ const initialValues = {
   height: "",
   weight: "",
 };
+
+const validationSchema = yup.object().shape({
+  age: yup
+    .number()
+    .typeError("only numbers")
+    .min(16, "16 is a minimum value")
+    .max(100, "100 is a maximum value")
+    .required("required"),
+  height: yup
+    .number()
+    .typeError("only numbers")
+    .min(140, "140 is a minimum value")
+    .max(250, "250 is a maximum value")
+    .required("required"),
+  weight: yup
+    .number()
+    .typeError("only numbers")
+    .min(40, "40 is a minimum value")
+    .max(250, "250 is a maximum value")
+    .required("required"),
+});
 
 const BodyMeasurements = () => {
   const ctx = useContext(AuthContext);
@@ -21,6 +44,7 @@ const BodyMeasurements = () => {
         <h2>My Body</h2>
         <Formik
           initialValues={initialValues}
+          validationSchema={validationSchema}
           onSubmit={async (values) => {
             await setDoc(doc(db, `users/${uid}/body-details`, "details"), {
               gender: values.gender,
@@ -37,6 +61,7 @@ const BodyMeasurements = () => {
                 <option value="female">Female</option>
               </StyledField>
               <StyledField name="age" type="text" id="age" placeholder="age" />
+              <ErrorMessage name="age" component={ErrorMsg} />
             </Row>
             <StyledField
               name="height"
@@ -44,12 +69,14 @@ const BodyMeasurements = () => {
               id="height"
               placeholder="height"
             />
+            <ErrorMessage name="height" component={ErrorMsg} />
             <StyledField
               name="weight"
               type="weight"
               id="weight"
               placeholder="weight"
             />
+            <ErrorMessage name="weight" component={ErrorMsg} />
             <button type="submit">Save</button>
           </Form>
         </Formik>
