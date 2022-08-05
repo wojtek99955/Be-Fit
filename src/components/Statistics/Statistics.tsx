45579a791cd8f6d37ac43ccf0bd748c45412582d
Wojtek Książek <wojtek99955@gmail.com>
2022-08-05 11:40:12 +0200

    styles update

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import styled from "styled-components";
+
+export const Container = styled.section`
+  padding: 1rem;
+  width: 100%;
+  height: calc(100vh - 3.5rem);
+  background-color: white;
+  position: relative;
+  top: 3.5rem;
+  overflow-y: scroll;
+`;
 
 const Statistics = () => {
-  return <div>Statistics</div>;
+  return <Container>Statistics</Container>;
 };
 
 export default Statistics;
