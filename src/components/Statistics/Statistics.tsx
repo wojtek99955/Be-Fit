ab29e3f1032468c4513641d2033fdaf68785e302
Wojtek Książek <wojtek99955@gmail.com>
2022-08-05 13:43:56 +0200

    nutrients data added to the chart

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -4,6 +4,10 @@ import StatisticsPieChartIcon from "../../assets/svg/StatisticsPieChartIcon";
 import { Chart as ChartJS, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 import { Chart, ArcElement } from "chart.js";
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../AuthContext";
+import { getDoc, doc, collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
 
 export const Container = styled.section`
   padding: 1rem;
@@ -63,24 +67,73 @@ const Charts = styled.div`
   margin: 4rem 0;
 `;
 
-export const data = {
-  labels: ["Fat", "Carbo", "Protein", "Fiber"],
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [50, 19, 3, 5],
-      backgroundColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-      ],
-    },
-  ],
-};
-
 const Statistics = () => {
   ChartJS.register(ArcElement, Tooltip, Legend);
+
+  const [nutrients, setNutrients] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+  const ctx = useContext(AuthContext);
+  const uid = ctx?.currentUser.uid;
+
+  async function getNutrients() {
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    const foodRef = await collection(db, `users/${uid}/food`);
+    const docsSnap = await getDocs(foodRef);
+    const foodz: any = [];
+    await docsSnap.forEach((doc) => {
+      foodz.push(doc.data());
+    });
+    const filteredFoods = foodz.filter((item: any) => {
+      return item.month === month;
+    });
+
+    const consumedFat = filteredFoods.reduce((acc: any, obj: any) => {
+      return acc + obj.details.fat;
+    }, 0);
+    const consumedFiber = filteredFoods.reduce((acc: any, obj: any) => {
+      return acc + obj.details.fiber;
+    }, 0);
+    const consumedProtein = filteredFoods.reduce((acc: any, obj: any) => {
+      return acc + obj.details.protein;
+    }, 0);
+    const consumedCarbo = filteredFoods.reduce((acc: any, obj: any) => {
+      return acc + obj.details.carbo;
+    }, 0);
+    setNutrients({
+      fat: consumedFat.toFixed(1),
+      fiber: consumedFiber.toFixed(1),
+      protein: consumedProtein.toFixed(1),
+      carbo: consumedCarbo.toFixed(1),
+    });
+  }
+  useEffect(() => {
+    getNutrients();
+  }, []);
+
+  const data = {
+    labels: ["Fat", "Carbo", "Protein", "Fiber"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [
+          nutrients?.fat,
+          nutrients?.carbo,
+          nutrients?.protein,
+          nutrients?.fiber,
+        ],
+        backgroundColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+        ],
+      },
+    ],
+  };
 
   return (
     <Container>
