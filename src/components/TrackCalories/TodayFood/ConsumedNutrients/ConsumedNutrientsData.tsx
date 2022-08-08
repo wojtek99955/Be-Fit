4a32f4b1928debe1af6826402abd2807fb9b1b61
Wojtek Książek <wojtek99955@gmail.com>
2022-08-09 01:42:48 +0200

    dependencies arr error removed

--- src/components/TrackCalories/TodayFood/ConsumedNutrients/ConsumedNutrientsData.tsx
+++ src/components/TrackCalories/TodayFood/ConsumedNutrients/ConsumedNutrientsData.tsx
@@ -59,7 +59,7 @@ const ConsumedNutrientsData = ({ consumed, loading }: Props) => {
         }
       );
     }
-  }, [consumed, consumedNutrients, uid]);
+  }, []);
 
   return (
     <Consumed loading={loading}>
