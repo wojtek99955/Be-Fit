04f02b130e991c671029fb22e8bf7b31d2974eaa
Wojtek Książek <wojtek99955@gmail.com>
2022-08-08 12:11:24 +0200

    jumping rope calc added

---
+++ src/components/Calculators/JumpingRope.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import { Container, Wrapper, Text, FormWrapper } from "./CalculatorsStyle";
+import { Formik, Form, ErrorMessage, Field } from "formik";
+
+const JumpingRope = () => {
+  const initialValues = {
+    weight: "",
+    level: "",
+    duration: "",
+  };
+  return (
+    <Container>
+      <Wrapper>
+        <Text>
+          <h2>Jumping rope</h2>
+          <p>
+            Skipping is a full body exercise that incorporates all the
+            significant muscles of your body and can help you with overall
+            weight loss as well as targeted muscle growth. From increasing core
+            strength to leg strength, skipping targets the cellulite fat stored
+            in these tough weight loss areas while keeping the heart rate up to
+            add a cardiovascular exercise element.
+          </p>
+        </Text>
+
+        <FormWrapper>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={(val) => console.log(val)}
+          >
+            <Form>
+              <Field
+                name="weight"
+                type="number"
+                id="weight"
+                placeholder="weight"
+              />
+              <Field as="select" name="level">
+                <option value="8.8"> less then 100 skips</option>
+                <option value="11.8">100 - 120 skips</option>
+                <option value="12.3">120-160 skips</option>
+              </Field>
+              <Field
+                name="duration"
+                type="number"
+                id="duration"
+                placeholder="duration"
+              />
+              <button type="submit">Calculate</button>
+            </Form>
+          </Formik>
+        </FormWrapper>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default JumpingRope;
