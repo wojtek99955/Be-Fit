ec1cad3ac98e0f13ef326cd165b2f52856309a9c
Wojtek Książek <wojtek99955@gmail.com>
2022-08-11 20:34:34 +0200

    btns added

--- src/components/UpdateProfileModal/UpdateGoal/UpdateGoal.tsx
+++ src/components/UpdateProfileModal/UpdateGoal/UpdateGoal.tsx
@@ -1,4 +1,4 @@
-import { Title, Wrapper } from "../UpdateProfileModalStyle";
+import { Title, Wrapper, BtnsContainer } from "../UpdateProfileModalStyle";
 
 interface Props {
   setPage: React.Dispatch<React.SetStateAction<number>>;
@@ -8,6 +8,10 @@ const UpdateGoal = ({ setPage }: Props) => {
   return (
     <Wrapper>
       <Title>Set your goal</Title>
+      <BtnsContainer>
+        <button>Prev</button>
+        <button>Done</button>
+      </BtnsContainer>
     </Wrapper>
   );
 };
