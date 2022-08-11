56bf6ea7bd4e4a600df1141ddeff35a9850da1f6
Wojtek Książek <wojtek99955@gmail.com>
2022-08-11 21:18:28 +0200

    form update

--- src/components/UpdateProfileModal/UpdateGoal/UpdateGoal.tsx
+++ src/components/UpdateProfileModal/UpdateGoal/UpdateGoal.tsx
@@ -22,35 +22,51 @@ const initialValues = {
   targetWeight: "",
 };
 
+const validationSchema = yup.object().shape({
+  currentWeight: yup
+    .number()
+    .min(40, "40 is a minimum value")
+    .required("required"),
+  targetWeight: yup
+    .number()
+    .min(30, "30 is a minimum value")
+    .required("required"),
+});
+
 const UpdateGoal = ({ setPage, setShowModal }: Props) => {
   return (
     <Wrapper>
       <Title>Set your goal</Title>
       <Formik
         initialValues={initialValues}
+        validationSchema={validationSchema}
         onSubmit={(val) => console.log(val)}
       >
         <FormContainer>
-          <label htmlFor="currentWeight">Current weight</label>
-          <input type="number" id="currentWeight" name="currentWeight" />
-          <label htmlFor="targetWeight">Target weight</label>
-          <input type="number" id="targetWeight" name="targetWeight" />
-          <BtnsContainer>
-            <button
-              onClick={() => {
-                setPage(2);
-              }}
-            >
-              Prev
-            </button>
-            <button
-              onClick={() => {
-                setShowModal(false);
-              }}
-            >
-              Done
-            </button>
-          </BtnsContainer>
+          <Form>
+            <label htmlFor="currentWeight">Current weight</label>
+            <Field type="number" id="currentWeight" name="currentWeight" />
+            <ErrorMessage name="currentWeight" component={ErrorMsg} />
+            <label htmlFor="targetWeight">Target weight</label>
+            <Field type="number" id="targetWeight" name="targetWeight" />
+            <ErrorMessage name="targetWeight" />
+            <BtnsContainer>
+              <button
+                onClick={() => {
+                  setPage(2);
+                }}
+              >
+                Prev
+              </button>
+              <button
+                onClick={() => {
+                  setShowModal(false);
+                }}
+              >
+                Done
+              </button>
+            </BtnsContainer>
+          </Form>
         </FormContainer>
       </Formik>
     </Wrapper>
