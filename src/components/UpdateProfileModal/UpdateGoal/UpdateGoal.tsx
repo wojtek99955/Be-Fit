be7244a0cae0c8c9237c43c1bacc1cad40a32a3e
Wojtek Książek <wojtek99955@gmail.com>
2022-08-11 21:08:33 +0200

    formik added

--- src/components/UpdateProfileModal/UpdateGoal/UpdateGoal.tsx
+++ src/components/UpdateProfileModal/UpdateGoal/UpdateGoal.tsx
@@ -1,30 +1,58 @@
-import { Title, Wrapper, BtnsContainer } from "../UpdateProfileModalStyle";
+import {
+  Title,
+  Wrapper,
+  BtnsContainer,
+  FormContainer,
+} from "../UpdateProfileModalStyle";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useContext } from "react";
+import * as yup from "yup";
+import { ErrorMsg } from "../../Auth/AuthStyle";
+import { AuthContext } from "../../AuthContext";
+import { setDoc, doc } from "firebase/firestore";
+import { db } from "../../../firebase";
 
 interface Props {
   setPage: React.Dispatch<React.SetStateAction<number>>;
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+const initialValues = {
+  currentWeight: "",
+  targetWeight: "",
+};
+
 const UpdateGoal = ({ setPage, setShowModal }: Props) => {
   return (
     <Wrapper>
       <Title>Set your goal</Title>
-      <BtnsContainer>
-        <button
-          onClick={() => {
-            setPage(2);
-          }}
-        >
-          Prev
-        </button>
-        <button
-          onClick={() => {
-            setShowModal(false);
-          }}
-        >
-          Done
-        </button>
-      </BtnsContainer>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(val) => console.log(val)}
+      >
+        <FormContainer>
+          <label htmlFor="currentWeight">Current weight</label>
+          <input type="number" id="currentWeight" name="currentWeight" />
+          <label htmlFor="targetWeight">Target weight</label>
+          <input type="number" id="targetWeight" name="targetWeight" />
+          <BtnsContainer>
+            <button
+              onClick={() => {
+                setPage(2);
+              }}
+            >
+              Prev
+            </button>
+            <button
+              onClick={() => {
+                setShowModal(false);
+              }}
+            >
+              Done
+            </button>
+          </BtnsContainer>
+        </FormContainer>
+      </Formik>
     </Wrapper>
   );
 };
