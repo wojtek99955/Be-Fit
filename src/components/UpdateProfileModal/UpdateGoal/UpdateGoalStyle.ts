8c571e4d7fb17c5b944629b1d63cf3583a9f1cd8
Wojtek Książek <wojtek99955@gmail.com>
2022-08-12 15:50:52 +0200

    updateGoal style added

---
+++ src/components/UpdateProfileModal/UpdateGoal/UpdateGoalStyle.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const RangeInput = styled.div`
+  width: 100%;
+  position: relative;
+  margin-top: 1rem;
+
+  input {
+    -webkit-appearance: none;
+    width: 100%;
+    height: 0.5rem;
+    border-radius: 8px;
+    background: #d3d3d3;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: 0.2s;
+    transition: opacity 0.2s;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 1;
+    }
+    &::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      appearance: none;
+      height: 1.3rem;
+      width: 1.3rem;
+      border-radius: 50%;
+      background-color: #ffa101;
+    }
+    &::-moz-range-thumb {
+      width: 1.3rem;
+      height: 1.3rem;
+      background: #04aa6d;
+      appearance: none;
+      cursor: pointer;
+    }
+  }
+
+  div {
+    text-align: center;
+    margin: 2rem 0;
+    font-size: 1.3rem;
+  }
+`;
