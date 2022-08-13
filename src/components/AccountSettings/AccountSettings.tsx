aec670b4a84e870db769e7a360abe7e90ed0d45d
Wojtek Książek <wojtek99955@gmail.com>
2022-08-13 13:42:16 +0200

    styles update

--- src/components/AccountSettings/AccountSettings.tsx
+++ src/components/AccountSettings/AccountSettings.tsx
@@ -1,5 +1,14 @@
 import { useEffect } from "react";
 import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.section`
+  width: 100%;
+  height: calc(100vh - 3.5rem);
+  position: relative;
+  top: 3.5rem;
+  overflow-y: scroll;
+`;
 
 const AccountSettings = () => {
   const location = useLocation();
@@ -8,9 +17,9 @@ const AccountSettings = () => {
     location.pathname === "/settings" && navigation("/settings/account");
   }, []);
   return (
-    <>
+    <Container>
       <Outlet />
-    </>
+    </Container>
   );
 };
 
