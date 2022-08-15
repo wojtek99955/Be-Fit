41f065270abc37bd2e99c41e2283fdf4f90c0b13
Wojtek Książek <wojtek99955@gmail.com>
2022-08-15 14:21:37 +0200

    last month component added

---
+++ src/components/Home/LastMonth/LastMonth.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { Box } from "../CardStyles";
+const StyledBox = styled(Box)`
+  max-width: 1300px;
+  margin: 1rem auto;
+`;
+
+const LastMonth = () => {
+  return <StyledBox>LastMonth</StyledBox>;
+};
+
+export default LastMonth;
