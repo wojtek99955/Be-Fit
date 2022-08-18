ee74b1b714f9de27d2fef24c45e2312e05579ac8
Wojtek Książek <wojtek99955@gmail.com>
2022-08-19 00:14:16 +0200

    preferences component added

---
+++ src/components/AccountSettings/Preferences.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+const Container = styled.div`
+  padding-top: 10rem;
+`;
+const Preferences = () => {
+  return <Container>Preferences</Container>;
+};
+
+export default Preferences;
