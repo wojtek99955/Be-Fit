efa01a71fe95c6981be043bf847a9fa8fd523ffa
Wojtek Książek <wojtek99955@gmail.com>
2022-08-19 00:13:20 +0200

    preferences component added

--- src/components/AccountSettings/Email.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-const Container = styled.div`
-  padding-top: 10rem;
-`;
-const Email = () => {
-  return <Container>Email</Container>;
-};
-
-export default Email;
