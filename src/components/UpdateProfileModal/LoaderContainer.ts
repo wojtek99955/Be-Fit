2c0f230b323041020f847d7d88416be10f5b0f68
Wojtek Książek <wojtek99955@gmail.com>
2022-08-19 09:09:54 +0200

    loader container added

---
+++ src/components/UpdateProfileModal/LoaderContainer.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const LoaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 2rem;
+  position: absolute;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+`;
