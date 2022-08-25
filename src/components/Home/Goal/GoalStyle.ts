da1a1efcd81557f3044579864c5feffa29b2feed
Wojtek Książek <wojtek99955@gmail.com>
2022-08-25 09:40:24 +0200

    media queries update

--- src/components/Home/Goal/GoalStyle.ts
+++ src/components/Home/Goal/GoalStyle.ts
@@ -60,8 +60,8 @@ export const StyledBox = styled(Box)`
 `;
 export const StyledSettingsIcon = styled(SettingsIcon)`
   position: absolute;
-  top: 1rem;
-  right: 1rem;
+  top: 0.7rem;
+  right: 0.7rem;
 `;
 
 export const BoxWrapper = styled.div<WrapperProps>`
