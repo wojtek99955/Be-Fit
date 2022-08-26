0099c09ce4cec890a48f37a3001c6c387fed53ed
Wojtek Książek <wojtek99955@gmail.com>
2022-08-26 07:54:20 +0200

    mobile side bar added

---
+++ src/components/SideBarMobile/SideBarMobile.tsx
@@ -0,0 +1,107 @@
+import { useEffect, useContext, useState } from "react";
+import {
+  Container,
+  StyledNavLink,
+  HomeIcon,
+  GoalIcon,
+  CalculatorIcon,
+  FoodCalories,
+  StyledUserIcon,
+  User,
+  UserData,
+  WeightIcon,
+} from "./SideBarMobileStyle";
+import { db } from "../../firebase";
+import { doc, onSnapshot } from "firebase/firestore";
+import { AuthContext } from "../AuthContext";
+import { useLocation } from "react-router-dom";
+import SettingsSideBar from "../AccountSettings/SettingsSideBar";
+
+interface Props {
+  setShowSideBar: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowSideBarMobile: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const SideBarMobile = ({ setShowSideBar }: Props) => {
+  const ctx = useContext(AuthContext);
+  const uid = ctx?.currentUser.uid;
+  const [data, setData] = useState<any>({});
+
+  useEffect(() => {
+    onSnapshot(doc(db, `users/${uid}`), (doc) => {
+      setData(doc.data());
+    });
+  }, [uid]);
+
+  let location = useLocation();
+
+  return (
+    <>
+      {location.pathname === "/signin" ||
+      location.pathname === "/" ||
+      location.pathname === "/signup" ? null : (
+        <Container>
+          <User>
+            <StyledUserIcon url={data?.avatarImg}>
+              {data?.avatarImg ? null : data?.name?.toUpperCase().slice(0, 1)}
+            </StyledUserIcon>
+            <UserData>
+              <strong>
+                {data?.name?.length > 16
+                  ? `${data.name.slice(0, 17)}...`
+                  : data?.name}
+              </strong>
+              <span>
+                {data?.email?.length > 16
+                  ? `${data.email?.slice(0, 17)}...`
+                  : data?.email}
+              </span>
+            </UserData>
+          </User>
+          {location.pathname === "/settings/account" ||
+          location.pathname === "/settings/security" ||
+          location.pathname === "/settings/preferences" ? (
+            <SettingsSideBar />
+          ) : (
+            <nav>
+              <ul>
+                <li>
+                  <StyledNavLink to="/home">
+                    <HomeIcon />
+                    Home page
+                  </StyledNavLink>
+                </li>
+                <li>
+                  <StyledNavLink to="/my-goal">
+                    <GoalIcon />
+                    My goal
+                  </StyledNavLink>
+                </li>
+                <li>
+                  <StyledNavLink to="/calculators">
+                    <CalculatorIcon />
+                    Calculators
+                  </StyledNavLink>
+                </li>
+                <li>
+                  <StyledNavLink to="/calories-calculator">
+                    <FoodCalories />
+                    Check calories
+                  </StyledNavLink>
+                </li>
+                <li>
+                  <StyledNavLink to="/track-calories">
+                    <WeightIcon />
+                    Track Calories
+                  </StyledNavLink>
+                </li>
+              </ul>
+            </nav>
+          )}
+        </Container>
+      )}
+    </>
+  );
+};
+
+export default SideBarMobile;
---
+++ src/components/SideBarMobile/SideBarMobileStyle.ts
@@ -0,0 +1,110 @@
+import styled from "styled-components";
+import { BiHomeAlt } from "react-icons/bi";
+import { RiMedalLine } from "react-icons/ri";
+import { BiCalculator } from "react-icons/bi";
+import { MdOutlineFastfood } from "react-icons/md";
+import { NavLink } from "react-router-dom";
+import { MdOutlineMonitorWeight } from "react-icons/md";
+import { device } from "../../assets/mediaQueries/device";
+
+interface ImgProps {
+  url: string;
+}
+
+export const Container = styled.aside`
+  height: 100vh;
+  border-right: 1px solid #e1e4e7;
+  position: absolute;
+  top: 3.5rem;
+  height: calc(100vh - 3.5rem);
+  padding: 0.8rem;
+  z-index: 40;
+  width: 13rem;
+  background-color: white;
+  ul {
+    list-style-type: none;
+  }
+  li {
+    cursor: pointer;
+    font-weight: 300;
+  }
+  @media ${device.laptop} {
+    display: block;
+    position: relative;
+    top: 3.5rem;
+  }
+`;
+export const StyledNavLink = styled(NavLink)`
+  transition: background-color 300ms;
+  &:hover {
+    background-color: #f0f2f2;
+  }
+  &:active {
+    background-color: #e4e7e8;
+  }
+  &.active {
+    background-color: #e4e7e8;
+    font-weight: 600;
+  }
+  text-decoration: none;
+  color: black;
+  font-size: 0.9rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.8rem 0.5rem;
+  margin-bottom: 0.5rem;
+  border-radius: 5px;
+`;
+export const HomeIcon = styled(BiHomeAlt)`
+  font-size: 1.3rem;
+`;
+export const GoalIcon = styled(RiMedalLine)`
+  font-size: 1.3rem;
+`;
+export const CalculatorIcon = styled(BiCalculator)`
+  font-size: 1.3rem;
+`;
+export const FoodCalories = styled(MdOutlineFastfood)`
+  font-size: 1.3rem;
+`;
+export const StyledUserIcon = styled.div<ImgProps>`
+  width: 2rem;
+  height: 2rem;
+  background-color: #ffa101;
+  border-radius: 50%;
+  cursor: pointer;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  background-image: ${({ url }) => url && `url(${url})`};
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+export const User = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  margin-bottom: 1rem;
+  align-items: center;
+  height: 3rem;
+  strong {
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  span {
+    font-size: 0.9rem;
+    display: block;
+    color: #55595b;
+  }
+`;
+
+export const UserData = styled.div``;
+
+export const WeightIcon = styled(MdOutlineMonitorWeight)`
+  font-size: 1.3rem;
+`;
