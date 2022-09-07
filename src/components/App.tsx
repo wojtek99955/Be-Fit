0fb2f4bf75393967e58c5ee7da8565aa1c7eb523
Wojtek Książek <wojtek99955@gmail.com>
2022-09-07 11:24:52 +0200

    dark mode context added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,6 +6,7 @@ import SideBarMobile from "./SideBarMobile/SideBarMobile";
 import { AnimatePresence } from "framer-motion";
 import { MemoizedRoutes } from "./AppRoutes";
 import Background from "./SideBarMobile/Background";
+import { DarkModeContextProvider } from "../context/DarkModeContextProvider";
 
 function App() {
   const ctx = useContext(AuthContext);
@@ -21,30 +22,32 @@ function App() {
   });
 
   return (
-    <div className="App" style={{ display: "flex" }}>
-      <Header
-        setShowSideBar={setShowSideBar}
-        setShowSideBarMobile={setShowSideBarMobile}
-      />
+    <DarkModeContextProvider>
+      <div className="App" style={{ display: "flex" }}>
+        <Header
+          setShowSideBar={setShowSideBar}
+          setShowSideBarMobile={setShowSideBarMobile}
+        />
 
-      {islogged && showSideBar && currentWidth >= 1024 ? <SideBar /> : null}
-      <AnimatePresence>
-        {islogged && currentWidth < 1024 ? (
-          <>
-            <SideBarMobile
-              showSideBarMobile={showSideBarMobile}
-              setShowSideBarMobile={setShowSideBarMobile}
-            />
-            <Background
-              showSideBarMobile={showSideBarMobile}
-              setShowSideBarMobile={setShowSideBarMobile}
-            />
-          </>
-        ) : null}
-      </AnimatePresence>
+        {islogged && showSideBar && currentWidth >= 1024 ? <SideBar /> : null}
+        <AnimatePresence>
+          {islogged && currentWidth < 1024 ? (
+            <>
+              <SideBarMobile
+                showSideBarMobile={showSideBarMobile}
+                setShowSideBarMobile={setShowSideBarMobile}
+              />
+              <Background
+                showSideBarMobile={showSideBarMobile}
+                setShowSideBarMobile={setShowSideBarMobile}
+              />
+            </>
+          ) : null}
+        </AnimatePresence>
 
-      <MemoizedRoutes />
-    </div>
+        <MemoizedRoutes />
+      </div>
+    </DarkModeContextProvider>
   );
 }
 
