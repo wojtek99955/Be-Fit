4a0d1c0d93f15fca7d58c9199ee2e33aceed7837
Wojtek Książek <wojtek99955@gmail.com>
2022-09-07 17:44:18 +0200

    dark styles applied

--- src/components/AccountSettings/SettingsSideBar.tsx
+++ src/components/AccountSettings/SettingsSideBar.tsx
@@ -3,6 +3,12 @@ import { BiUserCircle } from "react-icons/bi";
 import { BiLock } from "react-icons/bi";
 import { HiOutlineMail } from "react-icons/hi";
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { darkModeContext } from "../../context/DarkModeContextProvider";
+
+interface DarkMode {
+  darkMode: boolean;
+}
 
 const UserIcon = styled(BiUserCircle)`
   font-size: 1.3rem;
@@ -14,20 +20,20 @@ const EmailIcon = styled(HiOutlineMail)`
   font-size: 1.3rem;
 `;
 
-const StyledNavLink = styled(NavLink)`
+const StyledNavLink = styled(NavLink)<DarkMode>`
   transition: background-color 300ms;
   &:hover {
-    background-color: #f0f2f2;
+    background-color: "ffa101";
   }
   &:active {
     background-color: #e4e7e8;
   }
   &.active {
-    background-color: #e4e7e8;
+    background-color: ${({ darkMode }) => (darkMode ? "#424243" : "#e4e7e8")};
     font-weight: 600;
   }
+  color: ${({ darkMode }) => (darkMode ? "white" : "black")};
   text-decoration: none;
-  color: black;
   font-size: 0.9rem;
   display: flex;
   align-items: center;
@@ -38,23 +44,25 @@ const StyledNavLink = styled(NavLink)`
 `;
 
 const SettingsSideBar = () => {
+  const darkModeCtx = useContext(darkModeContext);
+  const darkMode = darkModeCtx?.darkMode;
   return (
     <nav>
       <ul>
         <li>
-          <StyledNavLink to="/settings/account">
+          <StyledNavLink to="/settings/account" darkMode={darkMode!}>
             <UserIcon />
             Your account
           </StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to="/settings/security">
+          <StyledNavLink to="/settings/security" darkMode={darkMode!}>
             <LockIcon />
             Security
           </StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to="/settings/preferences">
+          <StyledNavLink to="/settings/preferences" darkMode={darkMode!}>
             <EmailIcon />
             Preferences
           </StyledNavLink>
