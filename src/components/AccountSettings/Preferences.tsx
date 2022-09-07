7cef0f0a7e56146e54d04cdd36c70d7a8bd29fd7
Wojtek Książek <wojtek99955@gmail.com>
2022-09-07 12:04:21 +0200

    toggle darkTheme added

--- src/components/AccountSettings/Preferences.tsx
+++ src/components/AccountSettings/Preferences.tsx
@@ -1,9 +1,31 @@
 import styled from "styled-components";
-const Container = styled.div`
-  padding-top: 10rem;
+import { useContext } from "react";
+import { darkModeContext } from "../../context/DarkModeContextProvider";
+import { Container } from "./AccountSettingsStyle";
+
+const ToggleModeBtn = styled.button`
+  padding: 0.7rem 2.8rem;
+  background-color: #ffa101;
+  border: none;
+  color: white;
 `;
+
 const Preferences = () => {
-  return <Container>Preferences</Container>;
+  const darkModeCtx = useContext(darkModeContext);
+  console.log(darkModeCtx?.darkMode);
+  const handleToggleDarkMode = () => {
+    darkModeCtx?.setDarkMode((prev) => !prev);
+  };
+  return (
+    <Container>
+      <h2>Preferences</h2>
+
+      <h3>Dark mode</h3>
+      <ToggleModeBtn onClick={handleToggleDarkMode}>
+        {darkModeCtx?.darkMode ? "On" : "Off"}
+      </ToggleModeBtn>
+    </Container>
+  );
 };
 
 export default Preferences;
