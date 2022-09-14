f13bb60670cb85ed8e2903a6cd711944c5605198
Wojtek Książek <wojtek99955@gmail.com>
2022-09-14 11:55:08 +0200

    props update

--- src/components/TrackCalories/TodayFood/TodayFood.tsx
+++ src/components/TrackCalories/TodayFood/TodayFood.tsx
@@ -97,8 +97,9 @@ const TodayFood = () => {
                       whileInView={{ x: 0, opacity: 1, scale: 1 }}
                       viewport={{ once: true }}
                       exit={{ x: "-40%", opacity: 0, scale: 0.5 }}
+                      darkMode={darkMode!}
                     >
-                      <FoodWrapper>
+                      <FoodWrapper darkMode={darkMode!}>
                         <FoodIconContainer>
                           <HealthyFoodIcon />
                         </FoodIconContainer>
