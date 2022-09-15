8582edf440b9f047ee33b02408d4c95ccc4a570b
Wojtek Książek <wojtek99955@gmail.com>
2022-09-15 10:44:33 +0200

    zindex update

--- src/components/AccountSettings/DeleteModal/DeleteModalStyle.ts
+++ src/components/AccountSettings/DeleteModal/DeleteModalStyle.ts
@@ -11,7 +11,7 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.5);
-  z-index: 10;
+  z-index: 50;
   top: 0;
   display: flex;
   justify-content: center;
