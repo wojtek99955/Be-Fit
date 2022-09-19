e50a885cd08bc98be7a5a05a1a186bf5fb1e7e05
wojtek99955 <wojtek99955@gmail.com>
2022-09-19 19:32:36 +0200

    no matched routes

---
+++ src/components/NoMatch/NoMatch.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.section`
+  margin-top: 5rem;
+`;
+const NoMatch = () => {
+  return <Container>NoMatch</Container>;
+};
+
+export default NoMatch;
