55bd73e8fabb15f1aba4bc53745b20e3a60ccf02
wojtek99955 <wojtek99955@gmail.com>
2022-09-19 19:40:13 +0200

    layout / content update

--- src/components/NoMatch/NoMatch.tsx
+++ src/components/NoMatch/NoMatch.tsx
@@ -1,10 +1,35 @@
 import styled from "styled-components";
+import { ImSad } from "react-icons/im";
 
 const Container = styled.section`
   margin-top: 5rem;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+const SadIcon = styled(ImSad)`
+  font-size: 3rem;
+  color: #ffa101;
+`;
+const Wrapper = styled.div`
+  max-width: 12rem;
+  h2 {
+    text-align: center;
+  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 const NoMatch = () => {
-  return <Container>NoMatch</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <h2>This address does not exists</h2>
+        <SadIcon />
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default NoMatch;
