f138b9c56db1e199b7fedaf0fc2fc085a906b440
wojtek99955 <wojtek99955@gmail.com>
2022-09-22 16:22:07 +0200

    redundant code removed

--- src/components/AccountSettings/AccountSettings.tsx
+++ src/components/AccountSettings/AccountSettings.tsx
@@ -1,20 +1,12 @@
 import { useEffect, useContext } from "react";
 import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import styled from "styled-components";
-import { darkModeContext } from "../../context/DarkModeContextProvider";
 
-interface DarkMode {
-  darkMode: boolean;
-}
-
-const Container = styled.section<DarkMode>`
+const Container = styled.section`
   width: 100%;
   height: auto;
   position: relative;
   top: 3.5rem;
-  transition: background-color 400ms;
-  background-color: ${({ darkMode, theme }) =>
-    darkMode ? theme.darkMode.main : " white"};
 `;
 
 const AccountSettings = () => {
@@ -25,10 +17,8 @@ const AccountSettings = () => {
     location.pathname === "/settings" && navigation("/settings/account");
   }, []);
 
-  const darkModeCtx = useContext(darkModeContext);
-  const darkMode = darkModeCtx?.darkMode;
   return (
-    <Container darkMode={darkMode!}>
+    <Container>
       <Outlet />
     </Container>
   );
