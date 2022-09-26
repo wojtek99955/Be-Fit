f8509330825c017e6561865a5e1cbe5495269d8d
wojtek99955 <wojtek99955@gmail.com>
2022-09-26 15:24:06 +0200

    svg added

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
+import StartShape from "../assets/svg/StartShape";
 
 const img = require("../assets/images/main.jpg");
 const Container = styled.div`
@@ -19,9 +20,46 @@ const Container = styled.div`
   width: 100%;
 `;
 
+const SvgContainer = styled.div`
+  position: absolute;
+`;
+
 const Start = () => {
   return (
     <Container>
+      <SvgContainer style={{ top: "-40%", width: "55%", height: "55%" }}>
+        <StartShape />
+      </SvgContainer>
+      <SvgContainer
+        style={{
+          bottom: "0%",
+          right: "5%",
+          width: "40%",
+          height: "40%",
+        }}
+      >
+        <StartShape />
+      </SvgContainer>
+      <SvgContainer
+        style={{
+          bottom: "10%",
+          right: "40%",
+          width: "25%",
+          height: "25%",
+        }}
+      >
+        <StartShape />
+      </SvgContainer>
+      <SvgContainer
+        style={{
+          bottom: "30%",
+          left: "10%",
+          width: "40%",
+          height: "40%",
+        }}
+      >
+        <StartShape />
+      </SvgContainer>
       <Outlet />
     </Container>
   );
