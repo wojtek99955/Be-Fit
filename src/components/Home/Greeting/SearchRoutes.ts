8f43a5cd36686d25dcdfe9da9126f11cdb23b6b0
wojtek99955 <wojtek99955@gmail.com>
2022-10-04 12:18:43 +0200

    routes update

--- src/components/Home/Greeting/SearchRoutes.ts
+++ src/components/Home/Greeting/SearchRoutes.ts
@@ -3,4 +3,28 @@ export const SearchRoutes = [
     name: "Home",
     route: "/home",
   },
+  {
+    name: "Bmi calculator",
+    route: "/calculators/body-calculators/bmi",
+  },
+  {
+    name: "Home",
+    route: "/home",
+  },
+  {
+    name: "Ideal weight calculator",
+    route: "/calculators/body-calculators/ideal-weight",
+  },
+  {
+    name: "Calorie intake calculator",
+    route: "/calculators/body-calculators/calorie-intake",
+  },
+  {
+    name: "Jumping rope calculator",
+    route: "/calculators/activity-calculators/jumping-rope",
+  },
+  {
+    name: "Running calculator",
+    route: "/calculators/activity-calculators/running",
+  },
 ];
