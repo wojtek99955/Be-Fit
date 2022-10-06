a9741b94613e44b0d5d6d5784a698f43168c2ab2
wojtek99955 <wojtek99955@gmail.com>
2022-10-06 15:14:22 +0200

    code refactor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,56 +11,14 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "../assets/styleTheme";
 import { useLocation } from "react-router-dom";
 import StartHeader from "./Header/StartHeader";
+import { darkModeContext } from "../context/DarkModeContextProvider";
+import AppLayout from "./AppLayout";
 
 function App() {
-  const ctx = useContext(AuthContext);
-  const islogged = ctx?.currentUser;
-  const [showSideBar, setShowSideBar] = useState(true);
-  const [showSideBarMobile, setShowSideBarMobile] = useState(false);
-  const [currentWidth, setCurrentWidth] = useState<number>(window.innerWidth);
-  useEffect(() => {
-    const setWidth = () => {
-      setCurrentWidth(window.innerWidth);
-    };
-    window.addEventListener("resize", setWidth);
-  });
-
-  let location = useLocation();
-
   return (
     <DarkModeContextProvider>
       <ThemeProvider theme={theme}>
-        <div className="App" style={{ display: "flex" }}>
-          {location.pathname === "/" ||
-          location.pathname === "/signup" ||
-          location.pathname === "/signin" ||
-          location.pathname === "/forgot-password" ? (
-            <StartHeader />
-          ) : (
-            <Header
-              setShowSideBar={setShowSideBar}
-              setShowSideBarMobile={setShowSideBarMobile}
-            />
-          )}
-
-          {islogged && showSideBar && currentWidth >= 1024 ? <SideBar /> : null}
-          <AnimatePresence>
-            {islogged && currentWidth < 1024 ? (
-              <>
-                <SideBarMobile
-                  showSideBarMobile={showSideBarMobile}
-                  setShowSideBarMobile={setShowSideBarMobile}
-                />
-                <Background
-                  showSideBarMobile={showSideBarMobile}
-                  setShowSideBarMobile={setShowSideBarMobile}
-                />
-              </>
-            ) : null}
-          </AnimatePresence>
-
-          <MemoizedRoutes />
-        </div>
+        <AppLayout />
       </ThemeProvider>
     </DarkModeContextProvider>
   );
