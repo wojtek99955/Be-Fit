a8d4a93aef39abc3bbe70c879aedf7b238dd7b36
wojtek99955 <wojtek99955@gmail.com>
2022-10-06 15:14:45 +0200

    props / styles update

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -4,37 +4,46 @@ import { MemoizedAppRoutesContainer } from "./AppRoutesContainer";
 import { darkModeContext } from "../context/DarkModeContextProvider";
 import { useContext } from "react";
 import { useLocation } from "react-router-dom";
+import { device } from "../assets/mediaQueries/device";
 
 interface StyleProps {
   darkMode: boolean;
   location: any;
+  showSideBar: boolean;
+  currentWidth: number;
 }
 
 const Container = styled.div<StyleProps>`
-  width: 100%;
-  height: 100vh;
-  overflow-y: ${({ location }) =>
+  width: ${({ showSideBar, currentWidth }) =>
+    showSideBar && currentWidth >= 1024 ? "calc(100% - 13rem)" : "100%"};
+  /* overflow-y: ${({ location }) =>
     location.pathname === "/" ||
     location.pathname === "/signup" ||
     location.pathname === "/signin" ||
     location.pathname === "/forgot-password"
       ? "hidden"
-      : "scroll"};
+      : "visible"}; */
   position: relative;
   transition: background-color 400ms;
   background-color: ${({ darkMode, theme }) =>
     darkMode ? theme.darkMode.main : "white"};
 `;
 
-interface AuthProps {
-  children: JSX.Element;
+interface Props {
+  showSideBar: boolean;
+  currentWidth: number;
 }
-const AppRoutes = () => {
+const AppRoutes = ({ showSideBar, currentWidth }: Props) => {
   const darkModeCtx = useContext(darkModeContext);
   const darkMode = darkModeCtx?.darkMode;
   let location = useLocation();
   return (
-    <Container darkMode={darkMode!} location={location}>
+    <Container
+      darkMode={darkMode!}
+      location={location}
+      showSideBar={showSideBar}
+      currentWidth={currentWidth}
+    >
       <MemoizedAppRoutesContainer />
     </Container>
   );
