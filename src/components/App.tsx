0336dc322cf736502421aaa503d3165037d1a492
wojtek99955 <wojtek99955@gmail.com>
2022-10-09 14:03:46 +0200

    App container added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,14 @@
-import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "./AuthContext";
-import Header from "./Header/Header";
-import SideBar from "./SideBar/SideBar";
-import SideBarMobile from "./SideBarMobile/SideBarMobile";
-import { AnimatePresence } from "framer-motion";
-import { MemoizedRoutes } from "./AppRoutes";
-import Background from "./SideBarMobile/Background";
 import { DarkModeContextProvider } from "../context/DarkModeContextProvider";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../assets/styleTheme";
-import { useLocation } from "react-router-dom";
-import StartHeader from "./Header/StartHeader";
-import { darkModeContext } from "../context/DarkModeContextProvider";
-import AppLayout from "./AppLayout";
+
+import AppContainer from "./AppContainer";
 
 function App() {
   return (
     <DarkModeContextProvider>
       <ThemeProvider theme={theme}>
-        <AppLayout />
+        <AppContainer />
       </ThemeProvider>
     </DarkModeContextProvider>
   );
